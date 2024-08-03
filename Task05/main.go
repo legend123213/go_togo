7c1e5fe1f588349ad2b650e13b0c62ea41b5d9fd
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-03 12:08:55 +0300

    feat: Update go.mod with latest dependencies refactor: main.go more module This commit updates the `go.mod` file in the `Task05` directory to include the latest versions of various dependencies.

---
+++ Task05/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/legend123213/go_togo/Task05/router"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+var MONGO_CLOUD_URL string
+
+
+func init(){
+	 err := godotenv.Load()
+    if err != nil {
+        fmt.Println("Error loading .env file")
+        return
+    }
+}
+func main(){
+	//importing api key for cloud mongo
+	MONGO_CLOUD_URL=os.Getenv("MONGO_CLOUD_URL")
+	
+	//initializer for mongo server
+	client := MongoStarter("TaskManger")
+
+
+	// initializer for mongo server
+	server:=router.Api(client)
+
+
+	server.Run(":8000")
+
+
+}
+
+func MongoStarter(dbname string) *mongo.Database{
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(MONGO_CLOUD_URL).SetServerAPIOptions(serverAPI)
+	client,err := mongo.Connect(context.TODO(),opts)
+	if err != nil {
+		log.Println("mongo server error")
+	}else{
+		log.Println("Mongo connected")
+	}
+	DB:=client.Database(dbname)
+	return DB
+}
