641a224f212ab12223abca1a642547b6ddf41163
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-03 12:10:54 +0300

    feat: Add data package for task management
    The code changes in this commit add a new `data` package in the `Task05` directory. This package includes the `task_server.go` file, which implements the `Tasks` interface for task management. The `data` package provides functionality for adding, editing, getting, and deleting tasks. This addition enhances the task management capabilities of the application.

---
+++ Task05/data/task_server.go
@@ -0,0 +1,44 @@
+package data
+
+import (
+	"context"
+	"log"
+
+	"github.com/legend123213/go_togo/Task05/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+
+
+type Tasks interface{
+	AddTask(task models.Task) (models.Task,error)
+	EditTask(task models.Task,id int) (models.Task,error)
+	GetTask(id int) (models.Task,error)
+	GetTasks() ([]models.Task,error)
+	DeleteTask(id int) error
+}
+
+
+
+
+
+func AddTask(task *models.Task,s *mongo.Database) (*models.Task,error){
+	store := s.Collection("Tasks")
+	data,err:=store.InsertOne(context.TODO(),*task)
+	id:=data.InsertedID.(primitive.ObjectID)
+	task.ID=id
+	return task,err
+}
+
+func GetTask(id string,s *mongo.Database) (*models.Task,error){
+	var task models.Task
+	ID,_ := primitive.ObjectIDFromHex(id)
+	log.Println(ID)
+	err := s.Collection("Tasks").FindOne(context.TODO(),bson.M{"_id": ID}).Decode(&task)
+	log.Println(err)
+	return &task,err
+
+}
