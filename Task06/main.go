541cf0f40ba84897f3eac46a22f8f509a8228164
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-06 14:41:37 +0300

    refactor: Update main.go to include MongoDB connection and environment variable loading
    This commit updates the main.go file to include the necessary code for connecting to MongoDB using the provided environment variable. It also adds the logic to load the environment variables from the .env file using the godotenv package. These changes ensure that the application can establish a connection to the MongoDB database and access the required environment variables.

--- Task06/main.go
+++ Task06/main.go
@@ -1,11 +1,44 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/legend123213/go_togo/Task06/router"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 
 
+func init(){
+	 err := godotenv.Load()
+    if err != nil {
+        log.Println("Error loading .env file")
+        return
+    }
+}
 
 func main(){
+	MONGO_CLOUD_URL:=os.Getenv("MONGO_CLOUD_URL")
+	dbmongo:=mongoService(MONGO_CLOUD_URL,"TaskManger")
+	server:=router.Api(dbmongo)
+	server.Run(":8000")
+
+}
 
 
-	
+func mongoService(url string,dbname string) *mongo.Database{
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(url).SetServerAPIOptions(serverAPI)
+	client,err := mongo.Connect(context.TODO(),opts)
+	if err != nil {
+		log.Println("mongo server error")
+	}else{
+		log.Println("Mongo connected")
+	}
+	DB:=client.Database(dbname)
+	return DB
 }
