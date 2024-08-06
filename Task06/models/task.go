152bfe04ba4fa3a8160010b5fe46f46e8c31bda0
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-06 14:42:53 +0300

    refactor: Add fields to Task struct in task.go
    The Task struct in task.go has been updated to include additional fields:
    - ID: represents the unique identifier for the task
    - Title: represents the title of the task
    - Description: represents the description of the task
    - Due_date: represents the due date of the task
    - Status: represents the status of the task
    - UserID: represents the user ID associated with the task
    
    These changes allow for more comprehensive task management within the system.

--- Task06/models/task.go
+++ Task06/models/task.go
@@ -1,8 +1,17 @@
 package models
 
+import (
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
-
+// Task represents a task in the system.
 type Task struct {
-	
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	Due_date     time.Time          `json:"due_date" bson:"due_date"`
+	Status      string             `json:"status" bson:"status"`
+	UserID      primitive.ObjectID  `bson:"_id," json:"id"`
 }
