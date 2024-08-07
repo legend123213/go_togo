a40953d70bc10b1765cefc3f1e94509004044907
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-07 15:50:16 +0300

    refactor: Add TaskServiceRepo struct and methods for task management
    This commit adds the TaskServiceRepo struct and its methods in the task_service.go file. The struct implements the TaskInterface interface, which defines the required methods for adding, getting, updating, and deleting tasks. These changes improve code organization and maintainability by separating the task management logic into a dedicated repository.

--- Task06/controllers/controller.go
+++ Task06/controllers/controller.go
@@ -1,40 +1,117 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/legend123213/go_togo/Task06/data"
+	"github.com/legend123213/go_togo/Task06/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+func isEmailUnique(ctx context.Context, collection *mongo.Collection, email string) error {
+    var existingUser models.User
+    err := collection.FindOne(ctx, bson.M{"email": email}).Decode(&existingUser)
+    if err == nil {
+        return fmt.Errorf("email '%s' already exists", email)
+    }
+    if err == mongo.ErrNoDocuments {
+        return nil 
+		}
 
+    return err
+}
 type UserControllerInter interface{
 	CreateUser(c *gin.Context)
 	GetUser(c *gin.Context)
 	GetAllUser(c *gin.Context)
 	UpdateUser(c *gin.Context)
 	RemoveUser(c *gin.Context)
+	LogUser(c *gin.Context)
+	MakeAdmin(c *gin.Context)
 }
 
 type Uc struct {
-	 Db  *mongo.Database
+	 DbStorage  *mongo.Database
+	 service data.UserServices
 }
 
 func NewUc(db *mongo.Database) *Uc{
+		var u data.UserServices = data.NewUserService()
 	return &Uc{
-		Db:db,
+		DbStorage:db,
+		service :u,
 	}
 }
 func (u *Uc)CreateUser(c *gin.Context){
+	storage := u.DbStorage
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := isEmailUnique(context.TODO(), u.DbStorage.Collection("Users"), user.Username); err != nil {
+        c.JSON(http.StatusConflict,gin.H{"message":err.Error() })
+		  return 
+    }
+
+	res,err := u.service.RegisterUser(&user,storage)
+	if err!=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": res})
+	}
+	c.JSON(http.StatusAccepted, gin.H{"message": "User registered in successfully", "token": res})
+
+}
+func (u *Uc) LogUser(c *gin.Context){
+	storage := u.DbStorage
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res,err := u.service.LoginUser(&user,storage)
+	if err!=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": res})
+		return 
+	}
+	c.JSON(http.StatusAccepted, gin.H{"message": "User login in successfully", "token": res})
 
 }
 func (u *Uc)UpdateUser(c *gin.Context){
 
 }
 func (u *Uc)GetUser(c *gin.Context){
+	id := c.Param("id")
+	store := u.DbStorage
+	user,err := u.service.GetUser(id,store)
+	if err != nil{
+		c.JSON(http.StatusNotFound,err)
+		return
+	}
+	c.JSON(http.StatusOK,user)
 
 }
 func (u *Uc)RemoveUser(c *gin.Context){
 
 }
 func (u *Uc)GetAllUser(c *gin.Context){
+	store:=u.DbStorage
+	service := u.service.GetAllUser(store)
+	c.JSON(http.StatusOK,service)
+
+}
+func (u *Uc)MakeAdmin(c *gin.Context){
+	id := c.Param("id")
+	store:=u.DbStorage
+	makeAdmin := u.service.RoleChanger(id,store)
+	if makeAdmin != nil{
+		c.JSON(http.StatusInternalServerError,gin.H{"error":"request is failed"})
+		return 
+	}
+	c.JSON(http.StatusAccepted,gin.H{"message":"user promoted"})
 
 }
 
@@ -49,26 +126,77 @@ type TaskControllerInter interface{
 }
 
 type Tc struct {
-	 Db  *mongo.Database
+	 DbStorage  *mongo.Database
+	 task data.TaskInterface
 }
 
-func NewTc(db *mongo.Database) *Uc{
-	return &Uc{
-		Db:db,
+func NewTc(db *mongo.Database) *Tc{
+	var t data.TaskInterface = data.NewTaskService()
+	return &Tc{
+		DbStorage:db,
+		task:t,
 	}
 }
-func (u *Uc)CreateTask(c *gin.Context){
+func (u *Tc)CreateTask(c *gin.Context){
+	storage :=u.DbStorage
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	data, err := u.task.SAddTask(&task, storage)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, "db error")
+		return
+	}
+	c.JSON(http.StatusAccepted, data)
 
 }
-func (u *Uc)UpdateTask(c *gin.Context){
+func (u *Tc)UpdateTask(c *gin.Context){
+	storage := u.DbStorage
+	var task models.Task
+	id := c.Param("id")
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	edited, errDb := u.task.SEditTask(id, storage, &task)
+	if errDb != nil {
+		c.JSON(http.StatusNotFound,gin.H{"Message":"task not found to be edited"})
+		return
+	}
+	c.JSON(http.StatusAccepted, edited)
 
 }
-func (u *Uc)GetTask(c *gin.Context){
+func (u *Tc)GetTask(c *gin.Context){
+	storage := u.DbStorage
+	id := c.Param("id")
+	data, err := u.task.SGetTask(id, storage)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message":"can't find the task"})
+		return
+	}
+	c.JSON(http.StatusOK, data)
 
 }
-func (u *Uc)RemoveTask(c *gin.Context){
+func (u *Tc)RemoveTask(c *gin.Context){
+	storage:= u.DbStorage
+	id := c.Param("id")
+	err :=u.task.SDeleteTask(id, storage)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message":"no task found to be delete"})
+		return
+	}
+	c.JSON(http.StatusNoContent, gin.H{"message": "Successfully Deleted"})
 
 }
-func (u *Uc)GetAllTask(c *gin.Context){
+func (u *Tc)GetAllTask(c *gin.Context){
+	storage := u.DbStorage
+	data, err := u.task.SGetTasks(storage)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+	c.JSON(http.StatusOK, data)
 
 }
--- Task06/data/task_service.go
+++ Task06/data/task_service.go
@@ -1,1 +1,93 @@
 package data
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/legend123213/go_togo/Task06/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+
+type TaskInterface interface{
+	SAddTask(task *models.Task,s *mongo.Database)(*models.Task,error)
+	SGetTask(id string,s *mongo.Database)(*models.Task, error)
+	SGetTasks(s *mongo.Database) ([]models.Task, error)
+	SDeleteTask(id string,s *mongo.Database) error
+	SEditTask(id string, s *mongo.Database, t *models.Task) (*models.Task, error)
+
+}
+
+type TaskServiceRepo struct{
+
+}
+
+func NewTaskService() *TaskServiceRepo{
+	return &TaskServiceRepo{
+		
+	}
+}
+
+// AddTask adds a new task to the database.
+func (this *TaskServiceRepo)SAddTask(task *models.Task, s *mongo.Database) (*models.Task, error) {
+	store := s.Collection("Tasks")
+	data, err := store.InsertOne(context.TODO(), *task)
+	id := data.InsertedID.(primitive.ObjectID)
+	task.ID = id
+	return task, err
+}
+// GetTask retrieves a task from the database based on the given ID.
+func  (this *TaskServiceRepo)SGetTask(id string, s *mongo.Database) (*models.Task, error) {
+	var task models.Task
+	ID, _ := primitive.ObjectIDFromHex(id)
+	err := s.Collection("Tasks").FindOne(context.TODO(), bson.M{"_id": ID}).Decode(&task)
+	return &task, err
+}
+
+// GetTasks retrieves all tasks from the database.
+func (this *TaskServiceRepo) SGetTasks(s *mongo.Database) ([]models.Task, error) {
+	var tasks []models.Task
+	iterDocument, err := s.Collection("Tasks").Find(context.TODO(), bson.D{})
+	for iterDocument.Next(context.TODO()) {
+		var task models.Task
+		if err := iterDocument.Decode(&task); err != nil {
+			return tasks, err
+		}
+		tasks = append(tasks, task)
+	}
+	return tasks, err
+}
+
+// DeleteTask deletes a task from the database based on the given ID.
+func (this *TaskServiceRepo) SDeleteTask(id string, s *mongo.Database) error {
+	ID, _ := primitive.ObjectIDFromHex(id)
+	check, err := s.Collection("Tasks").DeleteOne(context.TODO(), bson.M{"_id": ID})
+	if check.DeletedCount==0{
+		return errors.New("")
+	}
+	return err
+}
+
+// EditTask updates a task in the database based on the given ID.
+func  (this *TaskServiceRepo)SEditTask(id string, s *mongo.Database, t *models.Task) (*models.Task, error) {
+	ID, err_:= primitive.ObjectIDFromHex(id)
+	log.Println(err_)
+	update := bson.M{
+		"$set": bson.M{
+			"title":       t.Title,
+			"description": t.Description,
+			"due_date":    t.Due_date,
+			"status":      t.Status,
+		},
+	}
+	check, err := s.Collection("Tasks").UpdateOne(context.TODO(), bson.M{"_id": ID}, update)
+	t.ID = ID
+	if check.MatchedCount==0{
+		return t,errors.New("")
+	}
+	return t, err
+}
