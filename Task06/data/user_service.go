116def7cad479456f6ce08048f4c15fb11334f5e
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-07 15:50:26 +0300

    refactor: Update user_service.go to include JWT token generation and extraction
    This commit adds the necessary code in the user_service.go file to generate and extract JWT tokens. It includes the genratetoken function for generating tokens based on user credentials and the ExtractUserDataFromToken function for extracting user data from a token. These changes enhance the user authentication and authorization functionality of the system.

--- Task06/data/user_service.go
+++ Task06/data/user_service.go
@@ -1,44 +1,200 @@
 package data
 
 import (
+	"context"
+	"errors"
 	"log"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/legend123213/go_togo/Task06/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
+
+// Generate JWT
+func genratetoken(user *models.User,pwd string) (string,error){
+	var jwtSecret = []byte("your_jwt_secret")
+	log.Println(pwd,user.Password)
+	// User login logic
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pwd)) != nil {
+  return "Invalid username or password",nil
+			}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	"id":user.ID,
+  "role": user.IsAdmin,
+  "username":   user.Username,
+})
+
+jwtToken, err := token.SignedString(jwtSecret)
+return jwtToken,err
+   
+}
+
+
+
+func ExtractUserDataFromToken(tokenString string) (*models.User, error) {
+    var jwtSecret = []byte("your_jwt_secret")
+
+    // Parse the token
+    token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+        return jwtSecret, nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    // Extract the claims
+    if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		id := claims.ID
+		
+		user := &models.User{
+			ID:       id,
+			Username: claims.Username,
+			IsAdmin:  claims.IsAdmin,
+		}
+        return user, nil
+    }
+
+    return nil, errors.New("invalid token")
+}
+
+
+
+
+
 
 
 type UserServices interface{
-	AddUser(u *models.User,s *mongo.Database) error
-	EditUser(user *models.User,s *mongo.Database) *models.User
-	GetUser(id string,s *mongo.Database) *models.User
+	RegisterUser(u *models.User,s *mongo.Database) (string,error)
+	EditUser(id string,user *models.User,s *mongo.Database)(*models.User,error)
+	GetUser(id string,s *mongo.Database) (*models.User,error)
 	DeleteUser(id string,s *mongo.Database) error
+	LoginUser(u *models.User,s *mongo.Database) (string,error)
+	GetUserByUname(username string,s *mongo.Database)(*models.User,error)
+	RoleChanger(id string,s *mongo.Database)(error)
+	GetAllUser(s *mongo.Database) *[]models.User
 }
 
-type UserServiceRepo struct {
+type UserServiceRepo struct {}
 
 
+type Claims struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Username string `json:"username`
+	IsAdmin bool    `json:"isadmin"`
+	jwt.StandardClaims
 }
 func NewUserService() *UserServiceRepo{
 	return &UserServiceRepo{
 
 	}
 }
-func (u *UserServiceRepo)AddUser (user *models.User,s *mongo.Database) error{
-	log.Println(u,s,user)
-	return nil
+func (u *UserServiceRepo) RegisterUser(user *models.User,s *mongo.Database) (string,error){
+	store := s.Collection("Users")
+	pwd:=user.Password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+  		return "Internal server error",err
+	}
+	user.Password = string(hashedPassword)
+	cursor, errFound := store.CountDocuments(context.TODO(), bson.D{})
+	if errFound != nil {
+		log.Fatal(err)
+		return "mongo error",errFound
+	}
+	user.IsAdmin = true
+	if cursor != 0{
+		user.IsAdmin = false
+	}
+	
+	data,err_ := store.InsertOne(context.TODO(),*user)
+	if err_ != nil {
+		return "server error",err_
+	}
+	id := data.InsertedID.(primitive.ObjectID)
+	user.ID= id
+	validUser,err := u.GetUser(id.Hex(),s)
+	if err!=nil{
+		return "user not found",err
+	}
+	token,err := genratetoken(validUser,pwd)
+	return token,err
 }
-func (u *UserServiceRepo)EditUser (user *models.User,s *mongo.Database) *models.User{
-	log.Println(u,s,user)
-	return nil
+func (u *UserServiceRepo) EditUser(id string,user *models.User,s *mongo.Database) (*models.User,error){
+	ID, err_:= primitive.ObjectIDFromHex(id)
+	log.Println(err_)
+	update := bson.M{
+		"$set": bson.M{
+			"username":   user.Username,
+		},
+	}
+	check, err := s.Collection("Tasks").UpdateOne(context.TODO(), bson.M{"_id": ID}, update)
+	user.ID = ID
+	if check.MatchedCount==0{
+		return user,errors.New("")
+	}
+	return user, err
 }
-func (u *UserServiceRepo)GetUser (id string,s *mongo.Database) *models.User{
+func (u *UserServiceRepo) GetUser(id string,s *mongo.Database) (*models.User,error){
 	var user models.User
-	log.Println(id,s,user)
-	return &models.User{}
+	ID, _ := primitive.ObjectIDFromHex(id)
+	err := s.Collection("Users").FindOne(context.TODO(), bson.M{"_id": ID}).Decode(&user)
+	return &user, err
 }
-func (u *UserServiceRepo)DeleteUser (id string,s *mongo.Database) error{
+func (u *UserServiceRepo) GetUserByUname(username string,s *mongo.Database)(*models.User,error){
+	var user models.User
+	err := s.Collection("Users").FindOne(context.TODO(), bson.M{"username": username}).Decode(&user)
+	return &user, err
+
+}
+func (u *UserServiceRepo) DeleteUser(id string,s *mongo.Database) error{
 	
 	log.Println(id,s,id)
 	return nil
+}
+func (u *UserServiceRepo) LoginUser(user *models.User,s *mongo.Database) (string,error){
+	pwd:=user.Password
+	User,err := u.GetUserByUname(user.Username,s)
+	if err !=nil{
+		return "user not found",errors.New("")
+	}
+
+	return genratetoken(User,pwd)
+}
+func (u *UserServiceRepo) RoleChanger(id string,s *mongo.Database)(error){
+	User,err := u.GetUser(id,s)
+	if err != nil {
+		return errors.New("user not found")
+	}
+	User.IsAdmin = true
+	update := bson.M{
+		"$set": bson.M{
+			"username": User.Username,
+			"role": User.IsAdmin,
+		},
+	}
+	check, err := s.Collection("Users").UpdateOne(context.TODO(), bson.M{"_id":User.ID}, update)
+	if err != nil {
+		log.Println(err)
+		return errors.New("")
+	}
+	if check.MatchedCount==0{
+		return errors.New("")
+	}
+	return nil
+}
+
+func(u *UserServiceRepo) GetAllUser(s *mongo.Database) *[]models.User{
+	var users []models.User
+	store,_ := s.Collection("Users").Find(context.TODO(),bson.D{})
+	for store.Next(context.TODO()){
+		var user models.User
+		store.Decode(&user)
+		users=append(users,user)
+	}
+	
+	return &users
 }
