cdf2fdb51baf561281477f47ae35c527075c0bf7
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-07 15:50:44 +0300

    refactor: Update auth_middleware.go to include JWT token validation and user data extraction

--- Task06/middleware/auth_middleware.go
+++ Task06/middleware/auth_middleware.go
@@ -1,7 +1,79 @@
 package middleware
 
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strings"
 
-func auth(){
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/legend123213/go_togo/Task06/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+type Claims struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Username string `json:"username`
+	IsAdmin bool    `json:"role"`
+	jwt.StandardClaims
+}
+
+func AuthMiddleware()gin.HandlerFunc{
+	return func(c *gin.Context) {
+
+	var jwtSecret = []byte("your_jwt_secret")
+	authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+	tokenString := strings.Split(authHeader, " ")[1]
+    // Parse the token
+    token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+        return jwtSecret, nil
+    })
+
+    if err != nil {
+        c.JSON(http.StatusForbidden,err)
+    }
+
+    // Extract the claims
+    if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		id := claims.ID
+		
+		user := &models.User{
+			ID:       id,
+			Username: claims.Username,
+			IsAdmin:  claims.IsAdmin,
+		}
+		c.Set("id",user.ID)
+		c.Set("username",user.Username)
+		c.Set("isActive",user.IsAdmin)
+      c.Next()
+    }
+
+    c.JSON(http.StatusForbidden,errors.New("invalid token")) 
+	 c.Abort()
+	 return 
+	}
 
 	
+}
+
+func AdminMiddleware() gin.HandlerFunc{
+	return func (c *gin.Context)  {
+		isadmin, _ := c.MustGet("isActive").(bool)
+		log.Println(isadmin)
+		if isadmin {
+			c.Next()
+			
+		}else{
+		c.JSON(http.StatusUnauthorized,gin.H{"error":"it is admin role"})
+		c.Abort()
+		return 
+		}
+		
+		
+	}
 }
