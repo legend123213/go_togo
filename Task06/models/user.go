71330a818fe9531d07fff4178ff5e12a8c477863
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-07 15:53:26 +0300

    feat: Update User struct in user.go with ID, Username, and IsAdmin fields
    The User struct in user.go has been updated to include the following fields:
    - ID: represents the unique identifier for the user
    - Username: represents the username of the user
    - IsAdmin: represents whether the user has admin privileges
    
    These changes enhance the user model by adding necessary fields for identification and authorization.

--- Task06/models/user.go
+++ Task06/models/user.go
@@ -1,8 +1,11 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 
 type User struct{
-	Email string `bson:"email" json:"email"`
-	Role string `bson:"role" json:"role"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Username string `bson:"username" json:"username"`
+	IsAdmin bool `bson:"role" json:"role"`
 	Password string `bson:"pwd" json:"-"`
 }
